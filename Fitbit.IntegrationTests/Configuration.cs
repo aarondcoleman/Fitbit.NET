2a81174c23c314e36e2963fcdd63de2a63ed582b
Martin Davy <martin.davy@gmail.com>
2014-01-30 09:07:56 -0500

    Merge from upstream

--- Fitbit.IntegrationTests/Configuration.cs
+++ Fitbit.IntegrationTests/Configuration.cs
@@ -8,8 +8,8 @@ namespace Fitbit.IntegrationTests
     public class Configuration
     {
         //STEP 1 of Auth
-        public readonly static string ConsumerKey = "a9e9e4f0c8ec491b918b9aebe3fa6b06";
-        public readonly static string ConsumerSecret = "c4c9745caa3a4a2e803e8bd87ee964d9";
+        public readonly static string ConsumerKey = "YOUR_CONSUMER_KEY_HERE";
+        public readonly static string ConsumerSecret = "YOUR_CONSUMER_SECRET_HERE";
         public readonly static string RequestTokenUrl = "http://api.fitbit.com/oauth/request_token";
         public readonly static string AccessTokenUrl = "http://api.fitbit.com/oauth/access_token";
         public readonly static string AuthorizeUrl = "http://api.fitbit.com/oauth/authorize";
@@ -19,8 +19,8 @@ namespace Fitbit.IntegrationTests
         public readonly static string TempAuthVerifier = "YOUR_TEMP_AUTH_VERIFIER_HERE";
 
         //STEP 3 Permanent Auth Tokens Here
-        public readonly static string AuthToken = "ae6714ba71ce4bffa77d5ae7279279e2";
-        public readonly static string AuthTokenSecret = "687bac0d3eedacde54b35b577c4f2016";
-        public readonly static string FitbitUserId = "26SCX3";
+        public readonly static string AuthToken = "YOUR_AUTH_TOKEN_HERE";
+        public readonly static string AuthTokenSecret = "YOUR_AUTH_TOKEN_SECRET_HERE";
+        public readonly static string FitbitUserId = "YOUR_FITBIT_USERID_HERE";
     }
 }

114eaf7ba64c21a92b94b38ad6190536cebef82e
Matthew Will <mwill@nebraskaglobal.com>
2014-02-10 13:23:12 -0600

    add test for getting body measurements

--- Fitbit.IntegrationTests/UserTests.cs
+++ Fitbit.IntegrationTests/UserTests.cs
@@ -38,7 +38,7 @@ namespace Fitbit.IntegrationTests
             Assert.IsNotNull(userFriends);
 
             Console.WriteLine("# of friends:" + userFriends.Count);
-            foreach(UserProfile friend in userFriends)
+            foreach (UserProfile friend in userFriends)
             {
                 Console.WriteLine("Friend:" + friend.DisplayName + " - " + friend.EncodedId);
             }
@@ -62,7 +62,7 @@ namespace Fitbit.IntegrationTests
         [Test]
         public void Retrieve_Intraday_Calories()
         {
-            IntradayData intradayData = client.GetIntraDayTimeSeries(IntradayResourceType.CaloriesOut, new DateTime(2012, 10, 1, 0, 0, 0), new TimeSpan(24,0,0));
+            IntradayData intradayData = client.GetIntraDayTimeSeries(IntradayResourceType.CaloriesOut, new DateTime(2012, 10, 1, 0, 0, 0), new TimeSpan(24, 0, 0));
 
             Assert.IsNotNull(intradayData);
             Assert.IsTrue(intradayData.DataSet.Count() == 1440);
@@ -81,7 +81,7 @@ namespace Fitbit.IntegrationTests
 
             Assert.IsNotNull(firstReportDate);
 
-            if(firstReportDate.HasValue)
+            if (firstReportDate.HasValue)
                 Console.WriteLine("User's First Tracker Sync Day:" + firstReportDate.ToString());
 
         }
@@ -163,7 +163,7 @@ namespace Fitbit.IntegrationTests
 
             List<TimeSeriesDataListInt.Data> dataNotZero = new List<TimeSeriesDataListInt.Data>();
 
-            foreach(var timeSeriesEvent in dataList.DataList)
+            foreach (var timeSeriesEvent in dataList.DataList)
             {
                 if (timeSeriesEvent.Value > 0)
                     dataNotZero.Add(timeSeriesEvent);
@@ -174,5 +174,18 @@ namespace Fitbit.IntegrationTests
             Assert.IsNotNull(dataList);
 
         }
+
+
+        [Test]
+        public void Retrieve_BodyMeasurements_Yesterday()
+        {
+            BodyMeasurements measurements = client.GetBodyMeasurements(DateTime.Today.AddDays(-1));
+
+            Assert.IsNotNull(measurements);
+            Assert.IsNotNull(measurements.Body);
+            Assert.IsNotNull(measurements.Goals);
+
+        }
+
     }
 }

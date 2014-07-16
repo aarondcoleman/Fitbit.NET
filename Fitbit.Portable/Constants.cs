10db09c6002cc83ae2bc5f5887803c5885eff59f
Adam Storr <westdiscgolf@icecubez.net>
2014-07-16 22:50:48 +0100

    Async Authenticator Implementation initial drop

---
+++ Fitbit.Portable/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Fitbit.Api.Portable
+{
+    internal class Constants
+    {
+        public const string BaseApiUrl = "https://api.fitbit.com/";
+        public const string TemporaryCredentialsRequestTokenUri = "oauth/request_token";
+        public const string TemporaryCredentialsAccessTokenUri = "oauth/access_token";
+        public const string AuthorizeUri = "oauth/authorize";
+        public const string LogoutAndAuthorizeUri = "oauth/logout_and_authorize";
+    }
+}

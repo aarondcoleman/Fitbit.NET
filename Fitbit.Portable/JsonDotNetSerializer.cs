ad8fd3290879854116aa312b94cd7f63fabe2e4d
Adam Storr <westdiscgolf@icecubez.net>
2014-07-16 22:40:04 +0100

    JsonDotNetSerializer and tests (also test project rename)

---
+++ Fitbit.Portable/JsonDotNetSerializer.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Fitbit.Api.Portable
+{
+    internal class JsonDotNetSerializer
+    {
+        public string RootProperty { get; set; }
+
+        public T Deserialize<T>(string data)
+        {
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return default(T);
+            }
+
+            JObject o = JObject.Parse(data);
+            if (o == null)
+            {
+                return default(T);
+            }
+
+            T result = string.IsNullOrWhiteSpace(RootProperty) ? o.ToObject<T>() : o[RootProperty].ToObject<T>();
+            return result;
+        }
+    }
+}

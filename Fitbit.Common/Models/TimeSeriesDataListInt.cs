ffa051e3c63c87ee7d9fa2b06e3225591e36deb5
Adam Storr <westdiscgolf@icecubez.net>
2014-08-21 18:17:39 +0100

    Move TimeSeriesDataListInt into Fitbit.Common

---
+++ Fitbit.Common/Models/TimeSeriesDataListInt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fitbit.Models
+{
+    public class TimeSeriesDataListInt
+    {
+        public List<Data> DataList { get; set; }
+
+        public class Data
+        {
+            public DateTime DateTime { get; set; }
+            public int Value { get; set; }
+        }
+
+    }
+
+//    public class DataValue
+//    {
+//        public TimeSeriesData Data { get; set; }
+//    }
+
+    
+
+
+}

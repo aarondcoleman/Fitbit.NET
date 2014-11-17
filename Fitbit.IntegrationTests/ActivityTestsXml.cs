e60d9e3cf9dd7bb69af06f64ceb7a31f4be307d5
Konstantin Kostov <dotnetmaster@live.com>
2014-11-17 12:56:10 +0100

    created xml and json integration tests

---
+++ Fitbit.IntegrationTests/ActivityTestsXml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fitbit.Models.Enums;
+using NUnit.Framework;
+using Fitbit.Api;
+using RestSharp;
+using Fitbit.Models;
+
+
+namespace Fitbit.IntegrationTests
+{
+    [TestFixture]
+    public class ActivityTestsXml : TestsBase
+    {
+        public ActivityTestsXml()
+            : base(ResponseType.Xml)
+        {
+            //base class initializes client
+        }
+
+        //protected FitbitClient client;
+
+        [Test]
+        public void Retrieve_Activity_Summary_For_Yesterday()
+        {
+            ActivitySummary activitySummary = client.GetDayActivitySummary(DateTime.Now.Subtract(new TimeSpan(24, 0, 0)));
+
+            Assert.IsNotNull(activitySummary);
+
+            Assert.IsNotNull(activitySummary.Steps);
+            Console.WriteLine("Steps: " + activitySummary.Steps);
+            
+            Assert.IsNotNull(activitySummary.SedentaryMinutes);
+            Console.WriteLine("SedentaryMinutes: " + activitySummary.SedentaryMinutes);
+
+            Assert.IsNotNull(activitySummary.Distances);
+        }
+
+        
+    }
+}
--- Fitbit.IntegrationTests/TestsBase.cs
+++ Fitbit.IntegrationTests/TestsBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Fitbit.Models.Enums;
 using NUnit.Framework;
 using Fitbit.Api;
 using RestSharp;
@@ -12,6 +13,16 @@ namespace Fitbit.IntegrationTests
 {
     public class TestsBase
     {
+        public TestsBase(ResponseType responseType)
+        {
+            RestClient restClient = new RestClient("https://api.fitbit.com");
+            client = new FitbitClient(Configuration.ConsumerKey,
+                                        Configuration.ConsumerSecret,
+                                        Configuration.AuthToken,
+                                        Configuration.AuthTokenSecret,
+                                        restClient, responseType);
+        }
+
         public TestsBase()
         {
             RestClient restClient = new RestClient("https://api.fitbit.com");

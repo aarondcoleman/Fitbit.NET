60f82afc4ed2a1d8aa3a2acf71b462aa0e4578ba
Aaron Coleman <aaroninsd@gmail.com>
2015-06-08 14:01:21 -0700

    beginnings of concrete implementation

---
+++ Fitbit.Portable/OAuth2Authorization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Fitbit.Api.Portable
+{
+    public class OAuth2Authorization : IAuthorization
+    {
+        public OAuth2Authorization(string bearerToken, string refreshToken)
+        {
+
+        }
+
+        public void SetAuthorizationHeader(HttpClient httpClient)
+        {
+            AuthenticationHeaderValue authenticationHeaderValue = new AuthenticationHeaderValue("Bearer", bearerToken); 
+            httpClient.DefaultRequestHeaders.Authorization = authenticationHeaderValue; 
+
+        }
+
+    }
+}

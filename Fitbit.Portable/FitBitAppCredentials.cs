8a4218e8ac686e563a8f124e18147c8689658511
Mauricio Aviles <mau@fitabase.com>
2016-02-03 10:29:01 -0800

    namespace cleanup

--- Fitbit.Portable/FitBitAppCredentials.cs
+++ Fitbit.Portable/FitBitAppCredentials.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Fitbit.Api.Portable
+﻿namespace Fitbit.Api.Portable
 {
     public struct FitbitAppCredentials
     {

a31e220ef5b379a49f4ca3c08160dc810a261b61
Mauricio Aviles <mau@fitabase.com>
2016-02-25 17:19:36 -0800

    Adds property
    Fix #151

--- Fitbit.Portable/FitbitRequestException.cs
+++ Fitbit.Portable/FitbitRequestException.cs
@@ -6,9 +6,12 @@ namespace Fitbit.Api.Portable
 {
     public class FitbitRequestException : FitbitException
     {
+        public HttpStatusCode StatusCode { get; set; }
+
         public FitbitRequestException(HttpStatusCode statusCode, IEnumerable<ApiError> errors)
             : base($"Request exception - see errors for more details - {statusCode}", errors)
         {
+            this.StatusCode = statusCode;
         }
     }
 }

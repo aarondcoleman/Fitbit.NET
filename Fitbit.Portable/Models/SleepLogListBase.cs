276f5aa49d931cf040c98fda681891e5c8d5ce54
Dwayne Love <dwayne@fitabase.com>
2017-07-10 16:19:24 -0700

    Added HasMorePages property to SleepLogListBase

--- Fitbit.Portable/Models/SleepLogListBase.cs
+++ Fitbit.Portable/Models/SleepLogListBase.cs
@@ -3,6 +3,8 @@
     public class SleepLogListBase
     {
         public Pagination Pagination { get; set; }
+        //Shortcut property to allow user to check for additional paged data
+        public bool HasMorePages => Pagination?.Next != null;
         public SleepLogDateRange[] Sleep { get; set; }
     }
 }

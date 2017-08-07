f8fdddf25c75d47c2ba609987fb38eeb03b12d5d
Alex Ghiondea <alex.ghiondea@gmail.com>
2017-08-07 13:17:41 -0700

    Replace the test project for the .NET Standard project with a .NET Core test project.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 dotnet restore Fitbit\FitBit.NetCore.sln
 dotnet build Fitbit\FitBit.NetCore.sln
-dotnet test FitBit.NetCore.Tests\FitBit.NetCore.Tests.csproj
+dotnet test FitBit.NetCore.Tests.NetCoreApp\FitBit.NetCore.Tests.NetCoreApp.csproj

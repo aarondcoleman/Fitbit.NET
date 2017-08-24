42393b4dd2f3683aa667cb7db757bfdc77ff5cba
dwayne <dwaynelavon@gmail.com>
2017-08-24 12:56:11 -0700

    Removed old intraday code

--- Fitbit.Portable/JsonDotNetSerializerExtensions.cs
+++ Fitbit.Portable/JsonDotNetSerializerExtensions.cs
@@ -84,43 +84,6 @@ namespace Fitbit.Api.Portable
             return friends.Children().Select(serializer.Deserialize<UserProfile>).ToList();           
         }
 
-
-
-
-
-        /// <summary>
-        /// GetTimeSeriesDataList has to do some custom manipulation with the returned representation
-        /// </summary>
-        /// <param name="serializer"></param>
-        /// <param name="timeSeriesDataJson"></param>
-        /// <returns></returns>
-        internal static HeartActivitiesIntraday GetHeartRateIntraday(this JsonDotNetSerializer serializer, DateTime date, string heartRateIntradayJson)
-        {
-            if (string.IsNullOrWhiteSpace(heartRateIntradayJson))
-            {
-                throw new ArgumentNullException("heartRateIntradayJson", "heartRateIntradayJson can not be empty, null or whitespace.");
-            }
-
-            var activitiesHeartIntraday = JToken.Parse(heartRateIntradayJson)["activities-heart-intraday"];
-            var dataset = activitiesHeartIntraday["dataset"];
-
-            var result = new HeartActivitiesIntraday
-            {
-                Dataset = (from item in dataset
-                           select new HeartRateDatasetInterval
-                           {
-                               Time = DateTime.Parse(date.ToString("yyyy-MM-dd") + " " + item["time"].ToString()), //here, maybe pass in the date so we have a full object of date and time
-                               Value = int.Parse(item["value"].ToString())
-                           }).ToList(),
-                DatasetInterval = Convert.ToInt32(activitiesHeartIntraday["datasetInterval"]),
-                DatasetType = activitiesHeartIntraday["datasetType"].ToString()
-
-            };
-
-            return result;
-        }
-
-
         internal static List<HeartActivitiesTimeSeries> GetHeartActivitiesTimeSeries(this JsonDotNetSerializer serializer, string heartActivitiesTimeSeries)
         {
             if (string.IsNullOrWhiteSpace(heartActivitiesTimeSeries))

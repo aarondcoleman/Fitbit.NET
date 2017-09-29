d0f7a7d18833d8f01ab41443a23845388a0a3f6a
Dwayne Love <dwayne@fitabase.com>
2017-09-29 18:39:36 +0000

    Adding the ability to init with JsonSerializerSettings that can be used to convert jsjon with time zones

--- Fitbit.Portable/JsonDotNetSerializer.cs
+++ Fitbit.Portable/JsonDotNetSerializer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Fitbit.Api.Portable
@@ -6,12 +7,12 @@ namespace Fitbit.Api.Portable
     internal class JsonDotNetSerializer
     {
         private readonly JsonSerializer _jsonSerializer;
-
-        public JsonDotNetSerializer()
+        private readonly JsonSerializerSettings _settings;
+        public JsonDotNetSerializer(JsonSerializerSettings settings = null)
         {
-            JsonSerializerSettings settings = new JsonSerializerSettings();
-            settings.Converters.Add(new EmptyDateToMinDateConverter());
-            _jsonSerializer = JsonSerializer.CreateDefault(settings);
+            _settings = settings ?? new JsonSerializerSettings();
+            _settings.Converters.Add(new EmptyDateToMinDateConverter());
+            _jsonSerializer = JsonSerializer.CreateDefault(_settings);
         }
 
         /// <summary>
@@ -27,6 +28,22 @@ namespace Fitbit.Api.Portable
                 return default(T);
             }
 
+            if (_settings.DateParseHandling == DateParseHandling.DateTimeOffset )
+            {
+                /* Json.NET conversions do not correctly handle datetimeoffsets by default.
+                 Instead they convert the offset into the local time of the server running the code.
+                 This is problematic for using Fitbit data sets that include the user's timezone info.
+                 In order to get around this, DateParseHandling.DateTimeOffset is set on the JsonConvert serializer.
+                 Unfortunately, Jtoken.Parse does not have this ability and can not be used for this purpose.
+                 On the flip side, JsonConvert does not seem to allow a RootProperty, so right now
+                 only one or the other can be used.*/
+                if (!string.IsNullOrWhiteSpace(RootProperty))
+                {
+                    throw new Exception("Root property not compatible with DateParseHandling.DateTimeOffset");
+                }
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+
             JToken o = JToken.Parse(data);
             return Deserialize<T>(o);
         }
@@ -39,6 +56,8 @@ namespace Fitbit.Api.Portable
             }
 
             T result = string.IsNullOrWhiteSpace(RootProperty) ? token.ToObject<T>(_jsonSerializer) : token[RootProperty].ToObject<T>(_jsonSerializer);
+
+            // T result = string.IsNullOrWhiteSpace(RootProperty) ? token.ToObject<T>(_jsonSerializer) : token[RootProperty].ToObject<T>(_jsonSerializer);
             return result;
         }
     }

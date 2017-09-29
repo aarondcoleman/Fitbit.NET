8b99612665f999b7aec746a9422a05ae6a2188ae
Dwayne Love <dwayne@fitabase.com>
2017-09-29 20:46:36 +0000

    New parse exception for time zones

---
+++ Fitbit.Portable/FitbitParseException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fitbit.Api.Portable
+{
+    public class FitbitParseException: Exception
+    {
+        private const string DefaultMessage = "Error occured while trying to parse JSON.";
+        public FitbitParseException(Exception ex, string message = DefaultMessage) : base(message, ex)
+        {
+            
+        }
+
+        public FitbitParseException(string message = DefaultMessage) : base(message)
+        {
+
+        }
+    }
+}

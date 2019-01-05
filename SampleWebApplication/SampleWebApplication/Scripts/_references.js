8a058af4f43ac411a9bf8913c2965ee97733340b
Adam Storr <astorr@gmail.com>
2019-01-05 20:12:12 +0000

    Add back in the sample web application. Renamed it to make it clearer and removed unrequired code to aid with getting started.

---
+++ SampleWebApplication/SampleWebApplication/Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.3.1.js" />
+/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="respond.js" />

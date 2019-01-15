28ba56486e295672439615e503a2cb05880dae85
Tim Sutor <tim@fitabase.com>
2019-01-15 16:10:02 -0700

    Added WeightGoal Fitbit model class

---
+++ Fitbit.Portable/Models/WeightGoal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fitbit.Models
+{
+    public class WeightGoal
+    {
+        public DateTime StartDate { get; set; }
+
+        public double StartWeight { get; set; }
+
+        public double Weight { get; set; }
+
+
+    }
+}

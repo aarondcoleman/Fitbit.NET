d8362482b510cb795f1889f7c46a90144eb1821e
Tim Sutor <tim@fitabase.com>
2019-01-15 11:20:01 -0700

    Added enum to handle getting/setting goals for different time periods

---
+++ Fitbit.Portable/Models/GoalPeriod.cs
@@ -0,0 +1,15 @@
+﻿using Fitbit.Api.Portable;
+
+namespace Fitbit.Models
+{
+    /// <summary>
+    /// All goal periods that can be queried from fitbit
+    /// </summary>
+    public enum GoalPeriod
+    {
+        [StringValue("daily")]
+        Daily,
+        [StringValue("weekly")]
+        Weekly
+    }
+}

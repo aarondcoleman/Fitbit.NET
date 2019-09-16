a01f1c9e7e8bbc92994a855d4500dbc5ce71c7d4
Srini Vasudevan <srini@gofundraise.com>
2019-09-16 20:25:35 +1000

    migrate the package references

--- Fitbit.NetCore/AssemblyInfo.cs
+++ Fitbit.NetCore/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -28,4 +29,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("2.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Fitbit.NetCore.Tests")]
+[assembly: InternalsVisibleTo("Fitbit.NetCore.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c57958b5c424bde54b11312ac6992323acfd7de5d861429259c1c3a08a5acc419653720e8d67b1b1c6f3411b2014e867787cde1a36e3bf0cccc85492020e4a12fe95a049eb426b75d8e64f2d82b593587d08cd9ae0fc0653b40b427fc0bee6a7824509838aa98f8b7260c52c3ba4f8d53f675d84c554eb1670f85344620507b0")]

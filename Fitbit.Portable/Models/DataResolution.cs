3f847a49e303ba885eccc1ee15d516f1ea8191ff
Brittany Schenk <brittany@fitabase.com>
2022-12-14 12:05:07 -0800

    Add reusable DataResolution enum

---
+++ Fitbit.Portable/Models/DataResolution.cs
@@ -0,0 +1,19 @@
+﻿using Fitbit.Api.Portable;
+
+namespace Fitbit.Models
+{
+    public enum DataResolution
+    {
+        [StringValue("1sec")]
+        OneSecond = 0,
+
+        [StringValue("1min")]
+        OneMinute = 1,
+
+        [StringValue("5min")]
+        FiveMinute = 2,
+
+        [StringValue("15min")]
+        FifteenMinute = 3
+    }
+}

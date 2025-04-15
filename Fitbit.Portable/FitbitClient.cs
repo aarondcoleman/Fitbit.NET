904e1403916e1550a6db12f60657c206ff45f0e3
CatrinaFitabase <126000524+CatrinaFitabase@users.noreply.github.com>
2025-04-15 12:10:49 -0500

    Fix empty object returns, no root specified, for Breathing Rate.

--- Fitbit.Portable/FitbitClient.cs
+++ Fitbit.Portable/FitbitClient.cs
@@ -1768,8 +1768,16 @@ namespace Fitbit.Api.Portable
                     await HandleResponse(response);
 
                     string responseBody = await response.Content.ReadAsStringAsync();
-                    var seralizer = new JsonDotNetSerializer { RootProperty = "br" };
-                    return seralizer.Deserialize<List<BreathingRateSummary>>(responseBody);
+
+                    //removes all white space including new lines
+                    string responseBodyNoWhitespace = new string(responseBody.Where(c => !char.IsWhiteSpace(c)).ToArray());
+                    if (string.IsNullOrWhiteSpace(responseBody) || responseBodyNoWhitespace == "{}")
+                    {
+                        return new List<BreathingRateSummary>();
+                    }
+
+                    JsonDotNetSerializer serializer = new JsonDotNetSerializer { RootProperty = "br" };
+                    return serializer.Deserialize<List<BreathingRateSummary>>(responseBody);
                 }
             }
         }
@@ -1793,6 +1801,13 @@ namespace Fitbit.Api.Portable
                     await HandleResponse(response);
 
                     string responseBody = await response.Content.ReadAsStringAsync();
+                    //removes all white space including new lines
+                    string responseBodyNoWhitespace = new string(responseBody.Where(c => !char.IsWhiteSpace(c)).ToArray());
+                    if (string.IsNullOrWhiteSpace(responseBody) || responseBodyNoWhitespace == "{}")
+                    {
+                        return new List<BreathingRateIntraday>();
+                    }
+
                     var seralizer = new JsonDotNetSerializer { RootProperty = "br" };
                     return seralizer.Deserialize<List<BreathingRateIntraday>>(responseBody);
                 }
